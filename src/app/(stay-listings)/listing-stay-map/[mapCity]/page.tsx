e5b398a85fada6d8080e81dea789657e9660e660
Collins-II <collinsmambwe2nd@gmail.com>
2025-03-01 00:50:13 +0200

    resolution 102

--- src/app/(stay-listings)/listing-stay-map/[mapCity]/page.tsx
+++ src/app/(stay-listings)/listing-stay-map/[mapCity]/page.tsx
@@ -1,26 +1,24 @@
-import React, { FC } from "react";
+import React from "react";
 import SectionGridHasMap from "../../SectionGridHasMap";
 import { DEMO_STAY_LISTINGS } from "@/data/listings";
 import { StayDataType } from "@/data/types";
 
-export interface ListingStayMapPageProps {
-  params: {
-    mapCity: string;
-  };
-};
-
-const DEMO_STAYS = DEMO_STAY_LISTINGS
-
 export const dynamic = "force-dynamic"; // Temporary fix for debugging
 
-const ListingStayMapPage: FC<ListingStayMapPageProps> = async ({params}) => {
+interface ListingStayMapPageProps {
+  params: { mapCity: string };
+}
 
-     const listing = await DEMO_STAYS.filter((c) => c.province === params?.mapCity);
-  // Default flag if not found
-  
+const ListingStayMapPage = async ({ params }: ListingStayMapPageProps) => {
+  // Ensure mapCity exists
+  const city = params?.mapCity || "";
+
+  // Filter stays based on mapCity
+  const listing = DEMO_STAY_LISTINGS.filter((c) => c.province === city);
+
   return (
     <div className="container pb-24 lg:pb-28 2xl:pl-10 xl:pr-0 xl:max-w-none">
-      <SectionGridHasMap data={listing as StayDataType[]} city={params?.mapCity} />
+      <SectionGridHasMap data={listing as StayDataType[]} city={city} />
     </div>
   );
 };
